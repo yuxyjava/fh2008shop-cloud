5ae33a7ae1942d62f8d57a71e1364247d1cf3b56
zhangsan <tt@126.com>
2021-06-10 12:30:23 +0800

    update shopMember main

--- shop-member-api/src/main/java/com/fh/shop/api/shopMemberApp.java
+++ shop-member-api/src/main/java/com/fh/shop/api/shopMemberApp.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class shopMemberApp {
 
     public static void main(String[] args) {
+        System.out.println("今天学习Git，Happy");
         SpringApplication.run(shopMemberApp.class, args);
     }
 }
